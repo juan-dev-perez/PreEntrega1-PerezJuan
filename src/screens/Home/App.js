a50aa30cdc73e40a8b31b87ea29670a7e12df0c3
Juan David Perez <juandevperez@gmail.com>
2022-10-20 00:52:08 -0300

    Ordenando carpetas y archivos

--- src/screens/Home/App.js
+++ src/screens/Home/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from '../../components/ItemListContainer/ItemListContainer';
+import NavBar from '../../components/NavBar/NavBar';
 
 function App() {
   return (
