d93928cbaea8c9ff5d7f652581596f7a697c1b3e
Juan David Perez <juandevperez@gmail.com>
2022-10-22 00:24:57 -0300

    avance en ItemDetail

--- src/components/Home/Item/Item.js
+++ src/components/Home/Item/Item.js
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom';
 import './Item.css';
 
 const Item = ({item}) => {
     return (
-        <div className='card'>
+        <Link className='card'>
             <img src={item.pictureUrl} className="img_card" alt={`imagen del producto ${item.title}`}/>
             <p className='title_card'>{item.title}</p>
             <p className='description_card'>{item.description}</p>
             <p className='price_card'>{item.price}</p>
             <p className='id_card'>Id: {item.id}</p>
-        </div>
+        </Link>
     )
 }
 
